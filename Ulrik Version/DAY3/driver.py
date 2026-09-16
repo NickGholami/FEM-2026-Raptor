8127b14c8490247526a65c321f590c1c217315db
Ulrik Jørgensen <ulrik0969@gmail.com>
2026-09-16 14:12:15 +0200

    Tilføjet Ulrik Mappe

---
+++ Ulrik Version/DAY3/driver.py
@@ -0,0 +1,18 @@
+# %%
+
+# Python driver for 41525 FEM basic Matlab code
+import os
+
+# Changing to the directory of this file
+os.chdir(os.path.dirname(__file__))
+print ("Current working dir : %s" % os.getcwd())
+
+# import FEA code module
+from src.fea import Fea
+
+# Define input file (the standard FEM course Matlab input format)
+input_file = 'Exercise3_1.m'
+# Perform FEA
+fea = Fea(input_file)
+
+# %%
