2beadf74b11a0e0b137cebf1e66c6aca72001a8b
Ulrik Jørgensen <ulrik0969@gmail.com>
2026-09-21 18:39:05 +0200

    minor

--- Ulrik Version/DAY1/driver.py
+++ Ulrik Version/DAY1/driver.py
@@ -11,8 +11,7 @@ print ("Current working dir : %s" % os.getcwd())
 from src.fea import Fea
 
 # Define input file (the standard FEM course Matlab input format)
-input_file = 'competition.m'
+input_file = 'example1.m'
 # Perform FEA
 fea = Fea(input_file)
-
 # %%
